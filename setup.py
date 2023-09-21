8e665bfecda98fb408be6e4a229386ecbc91cfed
Ivan <95343915+Ninefiveblade@users.noreply.github.com>
2023-09-21 22:21:57 +0300

    fixes

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='clickhouse_orm_extended',
+    name='clickhouse-orm-extended',
     version='2.1.4a0',
     author='Ivan Alexeev',
     author_email='ivan_alexeev@realytics.team',
     description='Extended version of infi.clickhouse-orm',
     url='https://github.com/Ninefiveblade/infi.clickhouse-orm-extended',
-    packages=['infi'],
+    packages=['infi.clickhouse-orm-extended'],
     python_requires='>=3.9',
     install_requires=[
         'iso8601 >= 0.1.12',
