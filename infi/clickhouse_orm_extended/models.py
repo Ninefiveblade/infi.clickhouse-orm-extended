4fbeca6da7d4143bbac8629ca94cdfcdcd889f97
Ivan <95343915+Ninefiveblade@users.noreply.github.com>
2023-09-21 22:49:20 +0300

    change package

--- infi/clickhouse_orm_extended/models.py
+++ infi/clickhouse_orm_extended/models.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+from typing import Union
 import sys
 from collections import OrderedDict
 from itertools import chain
@@ -7,13 +8,15 @@ from logging import getLogger
 import pytz
 
 from .fields import Field, StringField
-from .utils import parse_tsv, NO_VALUE, get_subclass_names, arg_to_sql, unescape
+from .utils import parse_tsv, NO_VALUE, get_subclass_names, arg_to_sql, unescape, comma_join
 from .query import QuerySet
 from .funcs import F
-from .engines import Merge, Distributed
+from .engines import (Merge, Distributed,
+                      DictionaryLifetime, SourceClickhouse,
+                      SourcePostgreSQL, FlatDictionaryLayout,
+                      HashedDictionaryLayout, ComplexKeyHashedDictionaryLayout)
 
 logger = getLogger('clickhouse_orm')
-
 
 
 class Constraint:
@@ -250,6 +253,245 @@ class ModelBase(type):
         if not hasattr(orm_fields, name):
             raise NotImplementedError('No field class for %s' % db_type)
         return getattr(orm_fields, name)()
+
+
+class Dictionary(metaclass=ModelBase):
+    '''
+    A base class for ORM models that represent ClickHouse dictionaries.
+    sample:
+    '''
+
+    source: Union[SourceClickhouse, SourcePostgreSQL]
+    layout: Union[FlatDictionaryLayout, HashedDictionaryLayout, ComplexKeyHashedDictionaryLayout]
+    lifetime: DictionaryLifetime
+    _database = None
+    _readonly = False
+    _system = False
+    primary_key: Union[str, tuple]
+
+    def __init__(self, **kwargs):
+        '''
+        Creates a model instance, using keyword arguments as field values.
+        Since values are immediately converted to their Pythonic type,
+        invalid values will cause a `ValueError` to be raised.
+        Unrecognized field names will cause an `AttributeError`.
+        '''
+        super(Dictionary, self).__init__()
+        # Assign default values
+        self.__dict__.update(self._defaults)
+        # Assign field values from keyword arguments
+        for name, value in kwargs.items():
+            field = self.get_field(name)
+            if field:
+                setattr(self, name, value)
+            else:
+                raise AttributeError('%s does not have a field called %s' % (self.__class__.__name__, name))
+
+    def __setattr__(self, name, value):
+        '''
+        When setting a field value, converts the value to its Pythonic type and validates it.
+        This may raise a `ValueError`.
+        '''
+        field = self.get_field(name)
+        if field and (value != NO_VALUE):
+            try:
+                value = field.to_python(value, pytz.utc)
+                field.validate(value)
+            except ValueError:
+                tp, v, tb = sys.exc_info()
+                new_msg = "{} (field '{}')".format(v, name)
+                raise tp.with_traceback(tp(new_msg), tb)
+        super(Dictionary, self).__setattr__(name, value)
+
+    def set_database(self, db):
+        '''
+        Sets the `Database` that this model instance belongs to.
+        This is done automatically when the instance is read from the database or written to it.
+        '''
+        # This can not be imported globally due to circular import
+        from .database import Database
+        assert isinstance(db, Database), "database must be database.Database instance"
+        self._database = db
+
+    def get_database(self):
+        '''
+        Gets the `Database` that this model instance belongs to.
+        Returns `None` unless the instance was read from the database or written to it.
+        '''
+        return self._database
+
+    @classmethod
+    def fields(cls, writable=False):
+        '''
+        Returns an `OrderedDict` of the model's fields (from name to `Field` instance).
+        If `writable` is true, only writable fields are included.
+        Callers should not modify the dictionary.
+        '''
+        # noinspection PyProtectedMember,PyUnresolvedReferences
+        return cls._writable_fields if writable else cls._fields
+
+    def get_field(self, name):
+        '''
+        Gets a `Field` instance given its name, or `None` if not found.
+        '''
+        return self._fields.get(name)
+
+    @classmethod
+    def table_name(cls) -> None:
+        '''
+        Returns the model's database table name. By default this is the
+        class name converted to lowercase. Override this if you want to use
+        a different dictionary name.
+        '''
+        return cls.__name__.lower()
+
+    @classmethod
+    def has_funcs_as_defaults(cls):
+        '''
+        Return True if some of the model's fields use a function expression
+        as a default value. This requires special handling when inserting instances.
+        '''
+        return cls._has_funcs_as_defaults
+
+    @classmethod
+    def reload_dictionary(cls, db):
+        '''
+        Reloads the dictionary from its source.
+        '''
+        if db.cluster_name is not None:
+            return 'SYSTEM RELOAD DICTIONARY `%s`.`%s` ON CLUSTER `%s`' % (db.db_name, cls.table_name(), db.cluster_name)
+        return 'SYSTEM RELOAD DICTIONARY `%s`.`%s`' % (db.db_name, cls.table_name())
+
+    @classmethod
+    def create_dictionary_sql(cls, db):
+        '''
+        Returns the SQL statement for creating a table for this model.
+        '''
+        assert cls.primary_key is None or type(cls.primary_key) in (list, tuple), 'primary_key must be a list or tuple'
+        if db.cluster_name is not None:
+            parts = ['CREATE DICTIONARY IF NOT EXISTS `%s`.`%s` ON CLUSTER `%s` (' % (db.db_name, cls.table_name(), db.cluster_name)]
+        else:
+            parts = ['CREATE DICTIONARY IF NOT EXISTS `%s`.`%s` (' % (db.db_name, cls.table_name())]
+        # Fields
+        items = []
+        for name, field in cls.fields().items():
+            items.append('    %s %s' % (name, field.get_sql(db=db)))
+        # Constraints
+        for c in cls._constraints.values():
+            items.append('    %s' % c.create_dictionary_sql())
+        # Indexes
+        for i in cls._indexes.values():
+            items.append('    %s' % i.create_dictionary_sql())
+        parts.append(',\n'.join(items))
+        # Engine
+        parts.append(')')
+        parts.append('PRIMARY KEY %s' % comma_join(cls.primary_key, stringify=True))
+        parts.append(cls.source.create_sql())
+        parts.append(cls.lifetime.create_sql())
+        parts.append(cls.layout.create_sql())
+        return '\n'.join(parts)
+
+    @classmethod
+    def drop_dictionary_sql(cls, db):
+        '''
+        Returns the SQL command for deleting this model's table.
+        '''
+        if db.cluster_name is not None:
+            return 'DROP DICTIONARY IF EXISTS `%s`.`%s` ON CLUSTER `%s`' % (db.db_name, cls.table_name(), db.cluster_name)
+        return 'DROP DICTIONARY IF EXISTS `%s`.`%s`' % (db.db_name, cls.table_name())
+
+    @classmethod
+    def from_tsv(cls, line, field_names, timezone_in_use=pytz.utc, database=None):
+        '''
+        Create a model instance from a tab-separated line. The line may or may not include a newline.
+        The `field_names` list must match the fields defined in the model, but does not have to include all of them.
+
+        - `line`: the TSV-formatted data.
+        - `field_names`: names of the model fields in the data.
+        - `timezone_in_use`: the timezone to use when parsing dates and datetimes. Some fields use their own timezones.
+        - `database`: if given, sets the database that this instance belongs to.
+        '''
+        values = iter(parse_tsv(line))
+        kwargs = {}
+        for name in field_names:
+            field = getattr(cls, name)
+            field_timezone = getattr(field, 'timezone', None) or timezone_in_use
+            kwargs[name] = field.to_python(next(values), field_timezone)
+
+        obj = cls(**kwargs)
+        if database is not None:
+            obj.set_database(database)
+
+        return obj
+
+    def to_tsv(self, include_readonly=True):
+        '''
+        Returns the instance's column values as a tab-separated line. A newline is not included.
+
+        - `include_readonly`: if false, returns only fields that can be inserted into database.
+        '''
+        data = self.__dict__
+        fields = self.fields(writable=not include_readonly)
+        return '\t'.join(field.to_db_string(data[name], quote=False) for name, field in fields.items())
+
+    def to_tskv(self, include_readonly=True):
+        '''
+        Returns the instance's column keys and values as a tab-separated line. A newline is not included.
+        Fields that were not assigned a value are omitted.
+
+        - `include_readonly`: if false, returns only fields that can be inserted into database.
+        '''
+        data = self.__dict__
+        fields = self.fields(writable=not include_readonly)
+        parts = []
+        for name, field in fields.items():
+            if data[name] != NO_VALUE:
+                parts.append(name + '=' + field.to_db_string(data[name], quote=False))
+        return '\t'.join(parts)
+
+    def to_db_string(self):
+        '''
+        Returns the instance as a bytestring ready to be inserted into the database.
+        '''
+        s = self.to_tskv(False) if self._has_funcs_as_defaults else self.to_tsv(False)
+        s += '\n'
+        return s.encode('utf-8')
+
+    def to_dict(self, include_readonly=True, field_names=None):
+        '''
+        Returns the instance's column values as a dict.
+
+        - `include_readonly`: if false, returns only fields that can be inserted into database.
+        - `field_names`: an iterable of field names to return (optional)
+        '''
+        fields = self.fields(writable=not include_readonly)
+
+        if field_names is not None:
+            fields = [f for f in fields if f in field_names]
+
+        data = self.__dict__
+        return {name: data[name] for name in fields}
+
+    @classmethod
+    def objects_in(cls, database):
+        '''
+        Returns a `QuerySet` for selecting instances of this model class.
+        '''
+        return QuerySet(cls, database)
+
+    @classmethod
+    def is_read_only(cls):
+        '''
+        Returns true if the model is marked as read only.
+        '''
+        return cls._readonly
+
+    @classmethod
+    def is_system_model(cls):
+        '''
+        Returns true if the model represents a system table.
+        '''
+        return cls._system
 
 
 class Model(metaclass=ModelBase):
